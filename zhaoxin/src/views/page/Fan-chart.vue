dd8cf5909c093819a3f2d92d281125dcfc41e20e
Hewenhao <1293219461@qq.com>
2023-08-11 19:14:37 +0800

    分组织统计情况get接口完成

--- zhaoxin/src/views/page/Fan-chart.vue
+++ zhaoxin/src/views/page/Fan-chart.vue
@@ -1,65 +1,84 @@
 <template>
-  <!-- <div class="container" style="display: flex; justify-content: center"> -->
+  <div class="container" style="display: flex; justify-content: center">
     <div ref="pie" style="display: flex; width: 100%"></div>
-  <!-- </div> -->
+  </div>
 </template>
 
 <script lang="ts">
-import {axiosInstances} from "../../api/axiosConfi";
 import { Pie } from "@antv/g2plot";
-import { ref } from "@vue/reactivity";
-// import { onMounted } from "@vue/runtime-core";
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
-export default {
+export default {  
   setup() {
     const pie = ref(null);
-    let piePlot = null;
-    const url='http://43.139.117.216:8100/user/departmentData?organizationId=1';
+    const url = "http://43.139.117.216:8100/user/departmentData?organizationId=1";
+    interface Department {
+      type: string;
+      value: number;
+    }
+    let arr: Department[] = [];
+    // let ann:Department[]=[
+    // {type:"shsh",value:1},
+    // {type:"shh",value:1},
+    // {type:"sh",value:1},
+    // {type:" hdaidhoaidjasiojdoiasdjoajddjasidoajsdiadjiadjoia",value:1},
+    // {type:"a",value:1},
+    // {type:"bb",value:1},
+    // {type:"cc",value:1},
+    // {type:"dd",value:1},
+    // {type:" h22",value:1},
+    // {type:" 11h",value:1},
+    // {type:" 1h",value:1},{type:" 1110h",value:1},{type:" 00111h",value:1},{type:" 11100h",value:1}
+    
 
-    interface department{
-      departmentName:String,
-      num:number,
-    };
+    // ]
 
-    let arr:department;
-      axiosInstances.get("/user/departmentData",{baseURL:url,headers: { 
-      Authorization: "Bearer "+"eyJraWQiOiJlMTFhNGUyMy05YzlkLTQzMzQtOWRiMS0yNDQ2NDFiNjI0N2QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyMDIxMTAwOTgxNzEiLCJhdWQiOiJtZXNzYWdpbmctY2xpZW50IiwibmJmIjoxNjkxNjUyNTk3LCJzY29wZSI6WyJtZXNzYWdlLnJlYWQiXSwiaXNzIjoiaHR0cDovLzQzLjEzOS4xMTcuMjE2Ojk4MjEiLCJleHAiOjE2OTE2OTU3OTcsImlhdCI6MTY5MTY1MjU5N30.sCAntFNJpzxcghPWif-rcEUpGEb_bUMQma7OfVR2bN5ZlVFRkzKUH5vMtlqRwCXphiAGIhMy62qkLLDZmLIYhI9uniw8xmomAjH57_CPDk0X3U61wHoH9wEP47PPqaKJ9hyApPYAMH9a46GcfLATcxcNz45eIgJK7kEnZqA6A9SXMogBVELGD5Bi5rscdVmC3jnYWODSTsWfFe156s_IBBNO8qoViPRHAKsxpTgq1cHkEE2RkgZzBb5uws9JvhjQ0gDEz_KMnt38a2obi-kgXIQcnNURNOako5LSlxNGInZ9J0bQ3GqnRP62LDHkOE2LmWaIsXzDQeAhErb-tnJrZA"
-   }})
-.then(function (res:ref) {
-  console.log(res.data);
-  arr=res.data.data.department;
-  console.log(arr[0].num);
-  initChart();
- 
-  // onMounted(initChart);
-})
-.catch(function (err) {});
-let initChart=()=>{      
-     piePlot = new Pie(pie.value, {
+    onMounted(() => {
+      axios
+        .get(url, {
+          headers: {
+            Authorization: "Bearer eyJraWQiOiJlMTFhNGUyMy05YzlkLTQzMzQtOWRiMS0yNDQ2NDFiNjI0N2QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyMDIxMTAwOTgxNzEiLCJhdWQiOiJtZXNzYWdpbmctY2xpZW50IiwibmJmIjoxNjkxNzQyMTY2LCJzY29wZSI6WyJtZXNzYWdlLnJlYWQiXSwiaXNzIjoiaHR0cDovLzQzLjEzOS4xMTcuMjE2Ojk4MjEiLCJleHAiOjE2OTE3ODUzNjYsImlhdCI6MTY5MTc0MjE2Nn0.rFxMnLJeXR1x8pzgOsktedxpYGsNox62IhbEeqV2q5wiEHHwxYb57ubruYxwAXbKmLzzgLEy_vn2kmUm_XMRWNUD_KjHdCQfcIQXNnwUd0ILlKtSbhzJDfc0mXKKFV4A4WdvWDhEkIs3-jhH0Q8DIW6oeJ9NLojpqX1IaFB9yGUy-ZvRbpDezmEemhIf9hvEMmEiSnxF3TASQHqqTU3TKODbsmGDJddRMic7iAz4V7XTY9WBUk6yb6VM8zkNPnF_Wz0tCV4YEtXj7je3VfBOdWXusA9cMRO-B2Yv4oPWjfgLaOmVr53T0DoKcJW8CbceNW54nxnVzOOBzYuLabZlgA",
+          },
+        })
+        .then((res) => {
+          arr = res.data.data.department;
+          console.log(arr);
+          // console.log(ann);
+          initChart();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    );
+
+    const initChart = () => { 
+      let data = arr;
+      // arr.map((item) => ({
+      //   type: item.departmentName,
+      //   value: item.num,
+      // }));
+
+      const piePlot = new Pie(pie.value, {
         appendPadding: 10,
-        data: [
-          { type: arr[0].departmentName , value: arr[0].num },
-          { type: arr[1].departmentName , value: arr[1].num },
-          { type: arr[2].departmentName , value: arr[2].num },
-          { type: arr[3].departmentName , value: arr[3].num },
-        ],
+        data,
         angleField: "value",
         colorField: "type",
         radius: 0.8,
         label: { style: { fontSize: 20 } },
         legend: {
-          position: "top-center",
-          offsetX: 310,
+          position: "top",
+          // offsetX: 310,
           marker: { style: { r: 10 } },
         },
         interactions: [{ type: "element-active" }],
       });
-       piePlot.render();
+      piePlot.render();
     };
-    
     return {
       pie,
     };
-  }, 
+  },
 };
 </script>
--- zhaoxin/src/views/page/Histogram.vue
+++ zhaoxin/src/views/page/Histogram.vue
@@ -14,19 +14,53 @@
 
 <script lang="ts">
 import { Column } from "@antv/g2plot";
+import axios from "axios";
 import { ref } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
 export default {
+  
   setup() {
-    const data = [
-      { type: "网站运维部", value: 4 },
-      { type: "信息化运维部", value: 2 },
-      { type: "行政秘书部", value: 2 },
-      { type: "网络运维部", value: 2 },
-    ];
-
     const hiss = ref(null);
+    const url = "http://43.139.117.216:8100/user/departmentData?organizationId=1";
+    interface Department {
+     
+      type: string;
+      value: number;
+    }
+    let arr: Department[] = [];
+
+    
+    
+    // const data = [
+    //   { type: "网站运维部", value: 4 },
+    //   { type: "信息化运维部", value: 2 },
+    //   { type: "行政秘书部", value: 2 },
+    //   { type: "网络运维部", value: 2 },
+    // ];
+    
+    
+    onMounted(() => {
+      axios
+        .get(url, {
+          headers: {
+            Authorization: "Bearer eyJraWQiOiJlMTFhNGUyMy05YzlkLTQzMzQtOWRiMS0yNDQ2NDFiNjI0N2QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyMDIxMTAwOTgxNzEiLCJhdWQiOiJtZXNzYWdpbmctY2xpZW50IiwibmJmIjoxNjkxNzQyMTY2LCJzY29wZSI6WyJtZXNzYWdlLnJlYWQiXSwiaXNzIjoiaHR0cDovLzQzLjEzOS4xMTcuMjE2Ojk4MjEiLCJleHAiOjE2OTE3ODUzNjYsImlhdCI6MTY5MTc0MjE2Nn0.rFxMnLJeXR1x8pzgOsktedxpYGsNox62IhbEeqV2q5wiEHHwxYb57ubruYxwAXbKmLzzgLEy_vn2kmUm_XMRWNUD_KjHdCQfcIQXNnwUd0ILlKtSbhzJDfc0mXKKFV4A4WdvWDhEkIs3-jhH0Q8DIW6oeJ9NLojpqX1IaFB9yGUy-ZvRbpDezmEemhIf9hvEMmEiSnxF3TASQHqqTU3TKODbsmGDJddRMic7iAz4V7XTY9WBUk6yb6VM8zkNPnF_Wz0tCV4YEtXj7je3VfBOdWXusA9cMRO-B2Yv4oPWjfgLaOmVr53T0DoKcJW8CbceNW54nxnVzOOBzYuLabZlgA",
+          },
+        })
+        .then((res) => {
+          arr = res.data.data.department;
+          console.log(arr);
+          // console.log(ann);
+          initChart();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    );
+
+
     let initChart = () => {
+      let data=arr;
       const Hisplot = new Column(hiss.value, {
         width: 500,
         data,
@@ -35,7 +69,7 @@ export default {
         yField: "value",
         color: ["#6294fa", "#63daab", "#657798", "#f7c122"],
         seriesField: "type",
-        legend: { offsetX: 35 },
+        legend: { position:"top"},
         meta: {
           type: {
             alias: "部门",
@@ -47,7 +81,7 @@ export default {
       });
       Hisplot.render();
     };
-    onMounted(initChart);
+    // onMounted(initChart);
     return {
       hiss,
     };
